7e06616137a09c1575cb2b68dafca2ff54ad41c7
Your name here <your_email_address@example.com>
2020-11-24 20:07:27 +0200

    add site

---
+++ root/precache-manifest.1b52c0bd7ae9c8c1526d9fec840f2776.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49397a8e24025634160cbef652e9a41",
+    "url": "/website/root/index.html"
+  },
+  {
+    "revision": "a1b819016c4e11b03b36",
+    "url": "/website/root/static/css/main.a5818445.chunk.css"
+  },
+  {
+    "revision": "365aadbc94db6b28289c",
+    "url": "/website/root/static/js/2.4696840b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website/root/static/js/2.4696840b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1b819016c4e11b03b36",
+    "url": "/website/root/static/js/main.db329d86.chunk.js"
+  },
+  {
+    "revision": "2b36b17901d742f807e3",
+    "url": "/website/root/static/js/runtime-main.39adf459.js"
+  },
+  {
+    "revision": "a6705a8ba68f6d2351db313e97b4a41e",
+    "url": "/website/root/static/media/1.a6705a8b.jpg"
+  },
+  {
+    "revision": "dd5d8a03fdf01e6ebf97eb1dafabfb6b",
+    "url": "/website/root/static/media/1827px-Ilja_Jefimowitsch_Repin_-_Reply_of_the_Zaporozhian_Cossacks_-_Yorck.dd5d8a03.jpg"
+  },
+  {
+    "revision": "289ecbbf32beaeb074f1c5776b8cbb74",
+    "url": "/website/root/static/media/Arrow.289ecbbf.svg"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/website/root/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/website/root/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "df8d807f4e4dccadc755919835314b49",
+    "url": "/website/root/static/media/close.df8d807f.svg"
+  },
+  {
+    "revision": "b7725c73a5ece425dba20f5e2c00fc2b",
+    "url": "/website/root/static/media/e13e11278a9c6cf7bde187e48da47565.b7725c73.png"
+  },
+  {
+    "revision": "343643e4ad81618eccb572efc41deca5",
+    "url": "/website/root/static/media/film-strip.343643e4.svg"
+  },
+  {
+    "revision": "ba0c77843e011ee4d5d2fb7855268640",
+    "url": "/website/root/static/media/photo-file.ba0c7784.svg"
+  },
+  {
+    "revision": "50863b5758b27a181975fb2f41aa1126",
+    "url": "/website/root/static/media/rubo-franc-kazaki-u-gornoy.50863b57.jpg"
+  }
+]);
